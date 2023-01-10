2fc3b5fe0c6f3a939044ac50644896b6c3086581
EE127F <andrekmbf@protonmail.com>
2023-01-10 12:57:08 +0100

    added simple rebase test

--- test/MooniRouterSwaps.t.sol
+++ test/MooniRouterSwaps.t.sol
@@ -41,18 +41,18 @@ contract TestMooniRouterSwap is DSTest {
       token2.approve(address(router), 9999999999*10**18);
       weth.approve(address(router), 9999999999*10**18);
 
-      rebaseToken0 = new MockSOHM(10**9,10**8); //10% rebase
-      rebaseToken1 = new MockSOHM(10**9,5*10**8); //50% rebase
-      rebaseToken2 = new MockSOHM(10**9,10**9); //100% rebase
+      rebaseToken0 = new MockSOHM(75*10**9,10**8); //10% rebase
+      rebaseToken1 = new MockSOHM(75*10**9,5*10**8); //50% rebase
+      rebaseToken2 = new MockSOHM(75*10**9,10**9); //100% rebase
 
       console.logUint(rebaseToken0.balanceOf(address(this)));
       rebaseToken0.approve(address(router),10**40);
       rebaseToken1.approve(address(router),10**40);
       rebaseToken2.approve(address(router),10**40);
 
-      rebaseToken0.mint(address(this),75**10*18);
-      rebaseToken1.mint(address(this),75**10*18);
-      rebaseToken2.mint(address(this),75**10*18);
+      rebaseToken0.mint(address(this),75*10**18);
+      rebaseToken1.mint(address(this),75*10**18);
+      rebaseToken2.mint(address(this),75*10**18);
 
       console.logUint(rebaseToken0.balanceOf(address(this)));
     }
@@ -98,7 +98,7 @@ contract TestMooniRouterSwap is DSTest {
     }
     function testSwapExactTokensForTokensRebase() public {
 
-         console.logUint(rebaseToken0.balanceOf(address(this)));
+        console.logUint(rebaseToken0.balanceOf(address(this)));
         (uint256 liquidity01) = router.addLiquidity(
             address(rebaseToken0),
             address(token0),
@@ -121,6 +121,7 @@ contract TestMooniRouterSwap is DSTest {
         );
         assertTrue(liquidity12>0);
 
+        rebaseToken0.mint(address(this),10**18);
          uint amountToken0Before = token0.balanceOf(address(this));
          uint amountToken2Before = token2.balanceOf(address(this));
          uint amountrebaseToken0Before = rebaseToken0.balanceOf(address(this));
@@ -129,9 +130,9 @@ contract TestMooniRouterSwap is DSTest {
         path[0] = address(rebaseToken0);
         path[1] = address(token0);
         path[2] = address(token2);
-        
 
-        router.swapExactTokensForTokens(25*10**18, 10**11, path, address(this), address(this));
+
+        router.swapExactTokensForTokens(10**18, 10**12, path, address(this), address(this));
 
          uint amountrebaseToken0After = rebaseToken0.balanceOf(address(this));
          uint amountToken2After = token2.balanceOf(address(this));
@@ -142,9 +143,142 @@ contract TestMooniRouterSwap is DSTest {
          console.logUint(amountrebaseToken0After);
          console.logUint(amountToken2After);
 
-
          assertTrue(amountrebaseToken0After < amountrebaseToken0Before);
          assertTrue(amountToken2After > amountToken2Before);
+    }
+    function testSwapExactTokensForTokensRebaseOnly() public {
+
+        console.logUint(rebaseToken0.balanceOf(address(this)));
+        (uint256 liquidity01) = router.addLiquidity(
+            address(rebaseToken0),
+            address(rebaseToken1),
+            75 ether,
+            75 ether,
+            75 ether,
+            75 ether,
+            address(this)
+        );
+        assertTrue(liquidity01>0);
+
+        rebaseToken1.mint(address(this),75*10**18);
+        (uint256 liquidity12) = router.addLiquidity(
+            address(rebaseToken1),
+            address(rebaseToken2),
+            75 ether,
+            75 ether,
+            75 ether,
+            75 ether,
+            address(this)
+        );
+        assertTrue(liquidity12>0);
+
+        rebaseToken0.mint(address(this),10**18);
+
+        uint amountRebaseToken0Before = rebaseToken0.balanceOf(address(this));
+        uint amountRebaseToken1Before = rebaseToken1.balanceOf(address(this));
+        uint amountRebaseToken2Before = rebaseToken2.balanceOf(address(this));
+
+        address[] memory path = new address[](3);
+        path[0] = address(rebaseToken0);
+        path[1] = address(rebaseToken1);
+        path[2] = address(rebaseToken2);
+
+
+        router.swapExactTokensForTokens(10**18, 10**12, path, address(this), address(this));
+
+        uint amountRebaseToken0After = rebaseToken0.balanceOf(address(this));
+        uint amountRebaseToken2After = rebaseToken2.balanceOf(address(this));
+
+         console.logUint(amountRebaseToken0Before);
+         console.logUint(amountRebaseToken2Before);
+         console.logString("after: ");
+         console.logUint(amountRebaseToken0After);
+         console.logUint(amountRebaseToken2After);
+
+        assertTrue(amountRebaseToken0After < amountRebaseToken0Before);
+        assertTrue(amountRebaseToken2After > amountRebaseToken2Before);
+
+        rebaseToken2.transfer(address(1),rebaseToken2.balanceOf(address(this)));
+        rebaseToken0.mint(address(this),10**18);
+
+        amountRebaseToken0Before = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2Before = rebaseToken2.balanceOf(address(this));
+       
+        rebaseToken0.rebase();
+        rebaseToken0.rebase();
+        rebaseToken0.rebase();
+        rebaseToken0.rebase();
+
+        router.swapExactTokensForTokens(10**18, 10**12, path, address(this), address(this));
+
+        amountRebaseToken0After = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2After = rebaseToken2.balanceOf(address(this));
+
+        assertTrue(amountRebaseToken0After < amountRebaseToken0Before);
+        assertTrue(amountRebaseToken2After > amountRebaseToken2Before);
+
+        
+
+        rebaseToken2.transfer(address(1),rebaseToken2.balanceOf(address(this)));
+        rebaseToken0.mint(address(this),10**18);
+
+        amountRebaseToken0Before = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2Before = rebaseToken2.balanceOf(address(this));
+       
+        rebaseToken1.rebase();
+        rebaseToken1.rebase();
+        rebaseToken1.rebase();
+        rebaseToken1.rebase();
+
+        router.swapExactTokensForTokens(10**18, 10**12, path, address(this), address(this));
+
+        amountRebaseToken0After = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2After = rebaseToken2.balanceOf(address(this));
+
+        assertTrue(amountRebaseToken0After < amountRebaseToken0Before);
+        assertTrue(amountRebaseToken2After > amountRebaseToken2Before);
+
+ 
+        rebaseToken2.transfer(address(1),rebaseToken2.balanceOf(address(this)));
+        rebaseToken0.mint(address(this),10**18);
+
+        amountRebaseToken0Before = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2Before = rebaseToken2.balanceOf(address(this));
+        
+        //output amount of token2 should remain the same as the total value stored in the pool is not changing
+        for(uint i = 0;i<30;i++){
+        rebaseToken2.rebase();
+        }
+
+        router.swapExactTokensForTokens(10**18, 10**12, path, address(this), address(this));
+
+        amountRebaseToken0After = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2After = rebaseToken2.balanceOf(address(this));
+
+        assertTrue(amountRebaseToken0After < amountRebaseToken0Before);
+        assertTrue(amountRebaseToken2After > amountRebaseToken2Before);
+
+  
+        rebaseToken2.transfer(address(1),rebaseToken2.balanceOf(address(this)));
+        rebaseToken0.mint(address(this),10**18);
+
+        amountRebaseToken0Before = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2Before = rebaseToken2.balanceOf(address(this));
+
+        //
+        for(uint i = 0;i<10;i++){
+        rebaseToken0.rebase();
+        }
+
+        router.swapExactTokensForTokens(10**18, 10**1, path, address(this), address(this));
+
+        amountRebaseToken0After = rebaseToken0.balanceOf(address(this));
+        amountRebaseToken2After = rebaseToken2.balanceOf(address(this));
+
+        assertTrue(amountRebaseToken0After > amountRebaseToken0Before);
+
+   
+         console.logUint(rebaseToken2.balanceOf(address(this)));
     }
    function testSwapExactETHForTokens() public {
         (uint256 liquidity01) = router.addLiquidity(
@@ -222,68 +356,5 @@ contract TestMooniRouterSwap is DSTest {
        assertTrue(amountToken1After < amountToken1Before);
        assertTrue(amountETHBefore < amountETHAfter );
     }
-    // function testSwapExactTokensForTokensRebase() public {
-    //    
-    //     rebaseToken0.mint(address(this),10**28);
-    //     rebaseToken0.approve(address(router),10**33);
-    //     token0.mint(address(this),10**29);
-    //     token1.mint(address(this),10**29);
-    //     token0.approve(address(router),10**29);
-    //     token1.approve(address(router),10**29);
 
-    //     uint amountrebaseToken0Before = rebaseToken0.balanceOf(address(this));
-    //     uint amountToken0Before= token0.balanceOf(address(this));
-    //     uint amountToken1Before= token1.balanceOf(address(this));
-    //     // uint amountrebaseToken1Before = rebaseToken1.balanceOf(address(this));
-    //     // uint amountrebaseToken2Before = rebaseToken2.balanceOf(address(this));
-
-    //     // uint amountrebaseToken0Before = 75*10**18;
-    //     // uint amountrebaseToken1Before = 75*10**18;
-    //     // uint amountrebaseToken2Before = 75*10**18;
-
-
-    //     (uint256 liquidity01) = router.addLiquidity(
-    //         address(rebaseToken0),
-    //         address(token0),
-    //         10**18, 
-    //         10**18, 
-    //         10**18, 
-    //         10**18, 
-    //         address(this)
-    //     );
-
-
-    //     assertTrue(liquidity01>0);
-    //     (uint256 liquidity12) = router.addLiquidity(
-    //         address(token0),
-    //         address(token1),
-    //         10**18, 
-    //         10**18, 
-    //         10**18, 
-    //         10**18, 
-    //         address(this)
-    //     );
-    //     assertTrue(liquidity12>0);
-
-    //     address[] memory path = new address[](3);
-    //     path[0] = address(rebaseToken0);
-    //     path[1] = address(token0);
-    //     path[2] = address(token1);
-    //    
-    //     // rebaseToken0.mint(address(this),5*10**29);
-    //     router.swapExactTokensForTokens(10**12, 10**3, path, address(this), address(this));
-
-    //      uint amountrebaseToken0After = rebaseToken0.balanceOf(address(this));
-    //      uint amountToken1After = token1.balanceOf(address(this));
-
-    //      console.logUint(amountrebaseToken0Before);
-    //      console.logUint(amountToken1Before);
-    //      console.logString("after: ");
-    //      console.logUint(amountrebaseToken0After);
-    //      console.logUint(amountToken1After);
-
-    //      // assertTrue(amountrebaseToken0After < amountrebaseToken0Before);
-    //      // assertTrue(amountToken1After > amountToken1Before);
-
-    // }
 }
